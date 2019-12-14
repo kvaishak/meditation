e0eb10d52d4a030b765c320c2e8467b95a740611
vaishak.k <vaishak.k@zohocorp.com>
2019-12-14 16:25:41 +0530

    app UI revamped using Bootstrap

--- js/main.js
+++ js/main.js
@@ -1,11 +1,10 @@
 const app = () => {
     const song = document.querySelector(".song");
-    const play = document.querySelector(".play");
-    const outline = document.querySelector(".moving-outline circle");
-    const video = document.querySelector(".vid-container video");
+    const play = document.querySelector(".play-pause");
+    const video = document.querySelector(".video");
 
     //sounds
-    const sounds = document.querySelectorAll(".sound-picker button");
+    const sounds = document.querySelectorAll('.song-select button')
 
     //time-display
     const timeDisplay = document.querySelector(".time-display");
@@ -13,14 +12,11 @@ const app = () => {
     //getting the time selection buttons;
     const timeSelect = document.querySelectorAll(".time-select button");
 
-    //get the length of the outline.
-    const outlineLength = outline.getTotalLength();
+    //getting the progress bar
+    const progressBar = document.querySelector(".progress-bar")
 
     //Duration
-    let fakeDuration = 300;
-
-    outline.style.strokeDasharray = outlineLength;
-    outline.style.strokeDashoffset = outlineLength;
+    let setDuration = 300;
 
     //pick different sound
     sounds.forEach(sound => {
@@ -39,29 +35,28 @@ const app = () => {
     //Select Time
     timeSelect.forEach(option => {
         option.addEventListener('click', function() {
-            fakeDuration = this.getAttribute('data-time');
-            timeDisplay.textContent = `${Math.floor(fakeDuration/60)} : ${ "0" +  Math.floor(fakeDuration%60)}`;
+            setDuration = this.getAttribute('data-time');
+            timeDisplay.textContent = `${Math.floor(setDuration/60)} : ${ "0" +  Math.floor(setDuration%60)}`;
         });
     });
-
 
     //function specific to play and stop soungs;
     const checkPlaying = song => {
         if (song.paused) {
             song.play();
             video.play();
-            play.src = "./svg/pause.svg";
+            play.src = "./icons/pause.svg";
         } else {
             song.pause();
             video.pause();
-            play.src = "./svg/play.svg";
+            play.src = "./icons/play.svg";
         }
     }
 
     // we can animate the circle
     song.ontimeupdate = () => {
         let currentTime = song.currentTime;
-        let elapsed = fakeDuration - currentTime;
+        let elapsed = setDuration - currentTime;
         let seconds = Math.floor(elapsed % 60);
         let minutes = Math.floor(elapsed / 60);
         if (seconds < 10) {
@@ -71,20 +66,20 @@ const app = () => {
             minutes = "0" + minutes;
         }
 
-        //Animate the Circle
-        let progress = outlineLength - ((currentTime / fakeDuration) * outlineLength);
-        outline.style.strokeDashoffset = progress;
+        //Animate the Progressbar
+        let progressPercentage = (currentTime / setDuration) * 100;
+        progressBar.style.width = progressPercentage + "%";
 
         //animate the text
         timeDisplay.textContent = `${minutes} : ${seconds}`;
 
-        if (currentTime >= fakeDuration) {
+        if (currentTime >= setDuration) {
             song.pause();
             song.currentTime = 0;
-            play.src = "./svg/play.svg";
+            play.src = "./icons/play.svg";
             video.pause();
         }
     }
-};
 
+};
 app();
